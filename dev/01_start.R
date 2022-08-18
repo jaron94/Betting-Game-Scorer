ba65b11e34f2eabd0262bc0a892caf17599def08
jaron94 <jonathan.aron94@gmail.com>
2022-08-18 09:32:16 +0100

    Edit and run `golem` dev/01_start.R

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "bgScorer", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_title = "Betting Game Scorer", # The Title of the package containing the App
+  pkg_description = "Shiny application to keep score in the Betting Game (AKA Contract Whist).", # The Description of the package containing the App
+  author_first_name = "Jonathan", # Your First Name
+  author_last_name = "Aron", # Your Last Name
+  author_email = "jonathan.aron94@gmail.com", # Your Email
+  repo_url = "https://github.com/jaron94/Betting-Game-Scorer" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
@@ -33,16 +33,12 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license() # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
-
-## Use git ----
-usethis::use_git()
 
 ## Init Testing Infrastructure ----
 ## Create a template for tests
@@ -50,14 +46,9 @@ golem::use_recommended_tests()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
+# golem::use_favicon() # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
-golem::use_utils_ui(with_test = TRUE)
-golem::use_utils_server(with_test = TRUE)
-
-# You're now set! ----
-
-# go to dev/02_dev.R
-rstudioapi::navigateToFile("dev/02_dev.R")
+# golem::use_utils_ui(with_test = TRUE)
+# golem::use_utils_server(with_test = TRUE)
