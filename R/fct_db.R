f85fef967bb99f0a65fe7300e1d653f10d79defd
Jonathan Aron <jonathan.aron94@gmail.com>
2022-08-18 13:10:17 +0100

    Use `styler` package to reformat

--- R/fct_db.R
+++ R/fct_db.R
@@ -1,39 +1,40 @@
 create_empty_db <- function() {
-  
   con <- DBI::dbConnect(RSQLite::SQLite(), "games.sqlite")
-  
-  players_tab <- data.frame(player_id = integer(),
-                            first_name = character(),
-                            last_name = character())
-  
+
+  players_tab <- data.frame(
+    player_id = integer(),
+    first_name = character(),
+    last_name = character()
+  )
+
   DBI::dbCreateTable(
     conn = con,
     name = "players",
     fields = players_tab
   )
-  
+
   gametimes_tab <- data.frame(
     game_id = integer(),
     game_datetime = character()
   )
-  
+
   DBI::dbCreateTable(
     conn = con,
     name = "gametimes",
     fields = gametimes_tab
   )
-  
+
   gameplayers_tab <- data.frame(
     game_id = integer(),
     player_id = character()
   )
-  
+
   DBI::dbCreateTable(
     conn = con,
     name = "gameplayers",
     fields = gameplayers_tab
   )
-  
+
   rounds_tab <- data.frame(
     round = integer(),
     player_id = integer(),
@@ -41,11 +42,10 @@ create_empty_db <- function() {
     tricks = integer(),
     score = integer()
   )
-  
+
   DBI::dbCreateTable(
     conn = con,
     name = "rounds",
     fields = rounds_tab
   )
-  
 }
