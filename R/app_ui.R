6e3d83841d8cceb9b7c024ce8999c65b14cdda36
jaron94 <jonathan.aron94@gmail.com>
2022-08-18 13:10:17 +0100

    Use `styler` package to reformat

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,21 +12,20 @@ app_ui <- function(request) {
     fluidPage(
       theme = shinythemes::shinytheme("readable"),
       titlePanel("Betting Game Scorer"),
-      
       sidebarLayout(
-        sidebarPanel(width = 2,
-                     h4(uiOutput("round_info")),
-                     uiOutput("betting"),
-                     shinyjs::hidden(uiOutput("playing"))
+        sidebarPanel(
+          width = 2,
+          h4(uiOutput("round_info")),
+          uiOutput("betting"),
+          shinyjs::hidden(uiOutput("playing"))
         ),
         mainPanel(
-          fluidRow(column(10, align = "center",
-                          tableOutput("play_table")
+          fluidRow(column(10,
+            align = "center",
+            tableOutput("play_table")
           ))
         )
       )
-      
-      
     )
   )
 }
@@ -61,17 +60,20 @@ card_seq <- function(round) {
 }
 
 # Wrapper around read_csv to suppress messages
-read_csv_q <- function(file) {suppressMessages(readr::read_csv(file))}
+read_csv_q <- function(file) {
+  suppressMessages(readr::read_csv(file))
+}
 
 # Function to create the modal dialog on startup to set up the game
 startup_modal <- function() {
   modalDialog(
     column(12, align = "center", h3("Game Set-up")),
-    column(12, align = "center", 
-           shinyWidgets::pickerInput("num_players", "How many players?", choices = 2:7)),
+    column(12,
+      align = "center",
+      shinyWidgets::pickerInput("num_players", "How many players?", choices = 2:7)
+    ),
     column(6, textInput("P1", "Who deals first?")),
     uiOutput("player_inputs"),
-    
     footer = tagList(
       actionButton("reload", "Reload Previous Game"),
       actionButton("set_up", "New Game")
@@ -81,15 +83,19 @@ startup_modal <- function() {
 
 # Function to create a modal dialog to display the scores at the end
 end_modal <- function() {
-  modalDialog(easyClose = TRUE,
-              fluidRow(
-                column(12, align = "center", 
-                       h4(textOutput("end_message")))
-              ),
-              fluidRow(
-                column(12, align = "center",
-                       tableOutput("final_scores")
-                )
-              )
+  modalDialog(
+    easyClose = TRUE,
+    fluidRow(
+      column(12,
+        align = "center",
+        h4(textOutput("end_message"))
+      )
+    ),
+    fluidRow(
+      column(12,
+        align = "center",
+        tableOutput("final_scores")
+      )
+    )
   )
 }
