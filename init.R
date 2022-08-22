05d2c4ece5a80671f391c1cdd826878eb25297e3
jaron94 <jonathan.aron94@gmail.com>
2022-08-22 19:50:59 +0100

    Use init.R to install deps on Heroku
    Renv cache makes the slug very large

---
+++ init.R
@@ -0,0 +1,14 @@
+helpers.installPackages("config",
+                        "dplyr",
+                        "golem",
+                        "kableExtra",
+                        "purrr",
+                        "readr",
+                        "shiny",
+                        "shinyjs",
+                        "shinythemes",
+                        "shinyWidgets",
+                        "tibble",
+                        "tidyr",
+                        "utils")
+
