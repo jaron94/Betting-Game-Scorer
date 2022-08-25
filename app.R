8935c078a48376943a3de7b1f1781859dbe747f9
Jonathan Aron <jonathan.aron@vivideconomics.com>
2022-08-25 16:26:22 +0100

    Deploy repo files instead of attempting to use built package
    shinyapps.io only recognises github package if installed via `remotes::install_github`

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 # Launch the ShinyApp (Do not remove this comment)
+
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
 # Set the Kable options to display missing values as empty strings
 options(knitr.kable.NA = '')
