fd5fa0afdd60454693a60607a07584d2798fb66c
Jonathan Aron <jonathan.aron@vivideconomics.com>
2022-08-25 16:02:42 +0100

    Attempt to fix shinyapps.io deployment
    Use app files (app.R, round.csv, table.csv) from repo rather than inst directory (no need for these to be included in the built package). Use `Sys.setenv()` instead of .Renviron to use production configuration.

--- inst/app.R
+++
@@ -1,4 +0,0 @@
-options( "golem.app.prod" = TRUE)
-# Set the Kable options to display missing values as empty strings
-options(knitr.kable.NA = '')
-bgScorer::run_app() # add parameters here (if any)
