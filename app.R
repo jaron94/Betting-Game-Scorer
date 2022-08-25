37d00fdc5faf9ecfd39df6019659c410dd912e4c
Jonathan Aron <jonathan.aron@vivideconomics.com>
2022-08-25 19:52:38 +0100

    Rationalise option definition in app.R files
    Move kable option to app_server.R, and supply `test.mode` as option within `run_app`

--- app.R
+++ app.R
@@ -1,8 +1,6 @@
 # Launch the ShinyApp (Do not remove this comment)
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
-options("golem.app.prod" = TRUE)
-# Set the Kable options to display missing values as empty strings
-options(knitr.kable.NA = '')
+options(golem.app.prod = TRUE)
 Sys.setenv(R_CONFIG_ACTIVE = "production")
 bgScorer::run_app()
--- tests/testthat/test_app/app.R
+++ tests/testthat/test_app/app.R
@@ -1,7 +1,4 @@
 # Launch the ShinyApp (Do not remove this comment)
 
-options("golem.app.prod" = TRUE)
-# Set the Kable options to display missing values as empty strings
-options(knitr.kable.NA = "")
-options(shiny.testmode = TRUE)
-bgScorer::run_app()
+options(golem.app.prod = TRUE)
+bgScorer::run_app(options = list(test.mode = TRUE))
