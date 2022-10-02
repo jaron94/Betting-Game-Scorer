17912fe91401a8cf169e51ae9e319f3baf8c7752
Jonathan Aron <jonathan.aron94@gmail.com>
2022-10-02 18:29:03 +0100

    Restyle package

--- tests/testthat/test_app/app.R
+++ tests/testthat/test_app/app.R
@@ -1,7 +1,7 @@
 # Launch the ShinyApp (Do not remove this comment)
 
-options( "golem.app.prod" = TRUE)
+options("golem.app.prod" = TRUE)
 # Set the Kable options to display missing values as empty strings
-options(knitr.kable.NA = '')
+options(knitr.kable.NA = "")
 options(shiny.testmode = TRUE)
 bgScorer::run_app()
