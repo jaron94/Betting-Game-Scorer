e11b31bac6162a069506bdb02edf5c8722237ee4
Jonathan Aron <jonathan.aron94@gmail.com>
2023-04-15 18:28:35 +0100

    Refactor tests and add reloading tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,13 +1,52 @@
 library(shinytest2)
 
+start_app <- function(
+    app_dir = test_path("test_app"),
+    variant = platform_variant(),
+    name = "Betting-Game-Scorer",
+    height = 569,
+    width = 979,
+    seed = 42,
+    ...
+) {
+  app <- AppDriver$new(
+    app_dir = app_dir,
+    variant = variant,
+    name = name,
+    height = height,
+    width = width,
+    seed = seed,
+    ...
+  )
+  
+  app$wait_for_value(input = "num_players")
+  
+  expect_equal(app$get_value(input = "num_players"), "2")
+  
+  app
+}
+
 setup_game <- function(app, players) {
   app$set_inputs(num_players = length(players))
   app$set_inputs(!!!players, allow_no_input_binding_ = TRUE)
   app$click("set_up")
+  app$wait_for_value(
+    input = paste0(players[1], "BR"),
+    ignore = list(NULL)
+  )
+  expect_equal(
+    app$get_values(input = paste0(players, "BR")) |>
+      purrr::flatten() |>
+      purrr::flatten_chr(),
+    rep("", length(players))
+  )
 }
 
 sim_bids <- function(app, players, valid = TRUE) {
   round <- app$get_value(export = "round")
+  
+  sweet_alert_button <- ".swal2-confirm"
+  sweet_alert_title <- ".swal2-title"
 
   tot_tricks <- card_seq(round)
 
@@ -30,10 +69,34 @@ sim_bids <- function(app, players, valid = TRUE) {
   app$set_inputs(!!!bids, allow_no_input_binding_ = TRUE)
 
   app$click("bet")
+  
+  if (valid) {
+    app$wait_for_value(
+      input = paste0(players[1], "PR"),
+      ignore = list(NULL)
+    )
+    
+    expect_null(app$get_html(sweet_alert_button))
+    
+    expect_equal(
+      app$get_values(input = paste0(players, "PR")) |>
+        purrr::flatten() |>
+        purrr::flatten_chr(),
+      rep("", length(players))
+    )
+  } else {
+    expect_equal(app$get_text(sweet_alert_title), "Error")
+    expect_equal(app$get_text(sweet_alert_button), "Ok")
+    
+    app$click(selector = sweet_alert_button)
+  }
 }
 
 sim_tricks <- function(app, players, valid = TRUE) {
   round <- app$get_value(export = "round")
+  
+  sweet_alert_button <- ".swal2-confirm"
+  sweet_alert_title <- ".swal2-title"
 
   tot_tricks <- card_seq(round)
 
@@ -56,106 +119,127 @@ sim_tricks <- function(app, players, valid = TRUE) {
   app$set_inputs(!!!tricks, allow_no_input_binding_ = TRUE)
 
   app$click("score")
+  
+  if (valid) {
+    app$wait_for_idle()
+    
+    loss_indicator <- app$get_html("body > div.swal2-container.swal2-center.swal2-backdrop-show > div > div.swal2-icon.swal2-info.swal2-icon-show")
+    
+    if (!is.null(loss_indicator)) {
+      app$click(selector = sweet_alert_button)
+    }
+    
+    app$wait_for_idle()
+    
+    expect_null(app$get_html(sweet_alert_button))
+  } else {
+    expect_equal(app$get_text(sweet_alert_title), "Error")
+    expect_equal(app$get_text(sweet_alert_button), "Ok")
+    
+    app$click(selector = sweet_alert_button)
+  }
 }
 
+google_auth_config_test <- function(app_dir = test_path("test_app"),
+                                    envir = parent.frame()) {
+  base_path <- file.path(app_dir, Sys.getenv("GOOGLE_CREDS_BASE"))
+  creds_path <- withr::local_file(file.path(app_dir, "creds.json"),
+                                  .local_envir = envir)
+  google_auth_config(base = base_path, complete = creds_path)
+  
+  withr::local_envvar(GOOGLE_APPLICATION_CREDENTIALS = creds_path,
+                      .local_envir = envir)
+  
+  expect_s3_class(bg_read_sheet("Completed"), "data.frame")
+}
+
+test_that("Google auth works", {
+  google_auth_config_test()
+})
 
 test_that("{shinytest2} recording: Betting-Game-Scorer", {
-  saved_game_dir <- get_saved_game_dir()
   
-  dir.create(test_path("test_app", saved_game_dir))
-  
-  on.exit(
-    {
-      unlink(
-        test_path("test_app", 
-                  c("table.csv", "round.csv", "game_id.csv", "creds.json"))
-      )
-      unlink(test_path("test_app", saved_game_dir), recursive = TRUE)
-    },
-    add = TRUE,
-    after = FALSE
-  )
-  
-  creds_path <- test_path("test_app", "creds.json")
+  sg_dir <- withr::local_tempdir()
+  withr::local_envvar(BG_GAMES_DIR = sg_dir)
 
-  google_auth_config(base = test_path("test_app", Sys.getenv("GOOGLE_CREDS_BASE")),
-                     complete = creds_path)
+  app <- start_app()
+  withr::defer(app$stop())
   
-  withr::with_envvar(
-    new = c(GOOGLE_APPLICATION_CREDENTIALS = creds_path),
-    expect_s3_class(bg_read_sheet("Completed"), "data.frame")
-  )
-
   players <- c(
     P1 = "Jon",
     P2 = "Tash",
     P3 = "Mum"
   )
 
-  app <- AppDriver$new(
-    app_dir = test_path("test_app"),
-    variant = platform_variant(),
-    name = "Betting-Game-Scorer",
-    height = 569, width = 979,
-    seed = 42
-  )
-
-  on.exit(app$stop(), add = TRUE, after = FALSE)
-  
-  app$wait_for_value(input = "num_players")
-
-  expect_equal(app$get_value(input = "num_players"), "2")
-
   setup_game(app, players)
 
-  app$wait_for_value(
-    input = paste0(players[1], "BR"),
-    ignore = list(NULL)
-  )
-
-  expect_equal(
-    app$get_values(input = paste0(players, "BR")) |>
-      purrr::flatten() |>
-      purrr::flatten_chr(),
-    rep("", length(players))
-  )
-
   sim_bids(app, players, valid = FALSE)
-
-  sweet_alert_button <- ".swal2-confirm"
-
-  expect_length(app$get_html(sweet_alert_button), 1)
-
-  app$click(selector = sweet_alert_button)
-
   sim_bids(app, players, valid = TRUE)
 
-  app$wait_for_value(
-    input = paste0(players[1], "PR"),
-    ignore = list(NULL)
-  )
-
-  expect_null(app$get_html(sweet_alert_button))
-
-  expect_equal(
-    app$get_values(input = paste0(players, "PR")) |>
-      purrr::flatten() |>
-      purrr::flatten_chr(),
-    rep("", length(players))
-  )
-
   sim_tricks(app, players, valid = FALSE)
-
-  expect_length(app$get_html(sweet_alert_button), 1)
-
-  app$click(selector = sweet_alert_button)
-
   sim_tricks(app, players, valid = TRUE)
-
-  expect_null(app$get_html(sweet_alert_button))
   
   for (i in seq(2, 12)) {
-    sim_bids(app, players, valid = TRUE)
-    sim_tricks(app, players, valid = TRUE)
+    sim_bids(app, players)
+    sim_tricks(app, players)
   }
+  
+  rlang::check_installed("rvest", "to test final scores")
+  
+  final_scores_ui <- app$get_value(output = "final_scores") |>
+    rvest::read_html() |>
+    rvest::html_element("table") |>
+    rvest::html_table() |>
+    tibble::as_tibble(.name_repair = "unique") |>
+    tibble::column_to_rownames("...1")
+  
+  game <- app$get_value(export = "game")
+  
+  final_scores_server <- game$calc_final_score()
+  
+  expect_equal(final_scores_ui, final_scores_server)
+})
+
+test_that("Reloading game works", {
+  
+  sg_dir <- withr::local_tempdir()
+  withr::local_envvar(BG_GAMES_DIR = sg_dir)
+  
+  app <- start_app()
+  
+  players <- c(
+    P1 = "Jon",
+    P2 = "Tash",
+    P3 = "Mum"
+  )
+  
+  setup_game(app, players)
+  
+  sim_bids(app, players)
+  
+  game <- app$get_value(export = "game")
+  
+  app$click("save_game")
+  app$stop()
+  
+  app <- start_app()
+  app$click("reload")
+  
+  loaded_game <- app$get_value(export = "game")
+  
+  expect_equal(game, loaded_game)
+  
+  sim_tricks(app, players)
+  
+  game <- app$get_value(export = "game")
+  
+  app$click("save_game")
+  app$stop()
+  
+  app <- start_app()
+  app$click("reload")
+  
+  loaded_game <- app$get_value(export = "game")
+  
+  expect_equal(game, loaded_game)
 })
