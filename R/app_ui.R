1538050f2e8dc53c241fa69c34544875a0008cd9
jaron94 <jonathan.aron94@gmail.com>
2023-05-01 12:05:54 +0100

    Initial commit of mobile_ui.R and app_mobile.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -61,7 +61,30 @@ golem_add_external_resources <- function() {
            margin-left: 0px;
            margin-right: 0px
       }")),
-    tags$script(jscode)
+    tags$script(jscode),
+    tags$script(
+      'Shiny.addCustomMessageHandler("ui-tweak", function(message) {
+                var os = message.os;
+                var skin = message.skin;
+                if (os === "md") {
+                  $("html").addClass("md");
+                  $("html").removeClass("ios");
+                  $(".tab-link-highlight").show();
+                } else if (os === "ios") {
+                  $("html").addClass("ios");
+                  $("html").removeClass("md");
+                  $(".tab-link-highlight").hide();
+                }
+
+                if (skin === "dark") {
+                 $(".view-main").addClass("theme-dark");
+                } else {
+                  $(".view-main").removeClass("theme-dark");
+                }
+
+               });
+              '
+    )
   )
 }
 
--- R/run_app.R
+++ R/run_app.R
@@ -24,3 +24,31 @@ run_app <- function(onStart = bg_gcs_auth, # nolint
     golem_opts = list(...)
   )
 }
+
+
+#' Run the Shiny Application on Mobile
+#'
+#' @param ... arguments to pass to golem_opts.
+#' See `?golem::get_golem_options` for more details.
+#' @inheritParams shiny::shinyApp
+#'
+#' @export
+#' @importFrom shiny shinyApp
+#' @importFrom golem with_golem_options
+run_app_mobile <- function(onStart = bg_gcs_auth, # nolint
+                           options = list(),
+                           enableBookmarking = NULL, # nolint
+                           uiPattern = "/", # nolint
+                           ...) {
+  with_golem_options(
+    app = shinyApp(
+      ui = mobile_ui,
+      server = mobile_server,
+      onStart = onStart,
+      options = options, # nolint undesirable_function_linter
+      enableBookmarking = enableBookmarking,
+      uiPattern = uiPattern
+    ),
+    golem_opts = list(...)
+  )
+}
