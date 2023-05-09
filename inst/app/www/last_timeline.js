cbe1853f3b85af42fd33e60775092a9585470890
jaron94 <jonathan.aron94@gmail.com>
2023-05-09 13:30:51 +0100

    Update last_timeline.js - style before and after

--- inst/app/www/last_timeline.js
+++ inst/app/www/last_timeline.js
@@ -2,10 +2,14 @@ $( document ).ready(function() {
   const bgcolor = window.getComputedStyle(
     document.getElementsByClassName("timeline-item-divider")[0]
   ).backgroundColor
-  const cssTemplateString = `#lasttime:before{background: ${bgcolor}}`;
-  const styleTag = document.createElement("style");
-  styleTag.innerHTML = cssTemplateString;
-  document.head.insertAdjacentElement('beforeend', styleTag);
+  const beforeTempstring = `#lasttime:before{background: ${bgcolor}}`;
+  const beforeTag = document.createElement("style");
+  beforeTag.innerHTML = beforeTempstring;
+  document.head.insertAdjacentElement('beforeend', beforeTag);
+  const afterTempstring = `#lasttime:after{background: ${bgcolor}}`;
+  const afterTag = document.createElement("style");
+  beforeTag.innerHTML = afterTempstring;
+  document.head.insertAdjacentElement('beforeend', beforeTag);
 });
 
 
