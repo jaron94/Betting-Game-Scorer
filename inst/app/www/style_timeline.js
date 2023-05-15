ba548ea6b549068350b81dd376c496b0729cc174
jaron94 <jonathan.aron94@gmail.com>
2023-05-15 11:38:01 +0100

    Fix style_timeline.js
    Correct typos and add `Shiny.addCustomMessageHandler` to allow invoking from server

--- inst/app/www/style_timeline.js
+++ inst/app/www/style_timeline.js
@@ -1,15 +1,19 @@
 $( document ).ready(function() {
-  const bgcolor = window.getComputedStyle(
-    document.getElementsByClassName("timeline-item-divider")[0]
-  ).backgroundColor
-  const beforeTempstring = `#lasttime:before{background: ${bgcolor}}`;
-  const beforeTag = document.createElement("style");
-  beforeTag.innerHTML = beforeTempstring;
-  document.head.insertAdjacentElement('beforeend', beforeTag);
-  const afterTempstring = `#lasttime:after{background: ${bgcolor}}`;
-  const afterTag = document.createElement("style");
-  beforeTag.innerHTML = afterTempstring;
-  document.head.insertAdjacentElement('beforeend', beforeTag);
+  Shiny.addCustomMessageHandler('restyle_tl_item_div', function(round) {
+    const bgcolor = window.getComputedStyle(
+      document.getElementsByClassName("timeline-item-divider")[0]
+    ).backgroundColor;
+    const tl_id = `tl_item${round}`;
+    document.getElementById(tl_id).style.backgroundColor = "red";
+    const beforeTempstring = `#${tl_id}:before{background: ${bgcolor}}`;
+    const beforeTag = document.createElement("style");
+    beforeTag.innerHTML = beforeTempstring;
+    document.head.insertAdjacentElement('beforeend', beforeTag);
+    const afterTempstring = `#${tl_id}:after{background: ${bgcolor}}`;
+    const afterTag = document.createElement("style");
+    afterTag.innerHTML = afterTempstring;
+    document.head.insertAdjacentElement('beforeend', afterTag);
+  })
 });
 
 
