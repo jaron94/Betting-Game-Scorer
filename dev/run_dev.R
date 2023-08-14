f2d97349bbf51667e25904c70fa591fd66d27c66
jaron94 <jonathan.aron94@gmail.com>
2023-08-14 11:40:30 +0200

    Adjust run_dev.R to use `chromote`

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -14,7 +14,14 @@ golem::document_and_reload()
 port <- getOption("shiny.port", 3838)
 host <- getOption("shiny.host", "127.0.0.1")
 
-appPath <- "app_mobile.R"
+device <- "iPhone 12"
+
+# Use app.R or app_mobile.R depending on the device
+appPath <- if (grepl("ipad", device)) {
+  "app.R"
+} else {
+  "app_mobile.R"
+}
 
 app_manager <<- shinybg:::app_manager # nolint: undesirable_operator_linter
 
@@ -37,12 +44,44 @@ app_manager$register_app(port, app)
 
 Sys.sleep(5)
 
-withr::with_options(
-  list(
-    browser = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" # nolint: absolute_path_linter
-  ),
-  shinyMobile::preview_mobile(
-    url = paste0("http://", host, ":", port),
-    device = "iphoneX"
-  )
+device_info_tab <- read.csv("dev/device_info.csv")
+landscape <- FALSE
+
+device_info <- device_info_tab |>
+  subset(device_name == device) |>
+  as.list()
+
+width <- device_info$width_viewport
+height <- device_info$height_viewport
+scale_factor <- device_info$dpr_js_css_pixel_ratio
+
+chrom <- chromote::Chromote$new()
+
+b <- chromote::ChromoteSession$new()
+
+# Start chrome in mobile emulation mode
+b$Emulation$setDeviceMetricsOverride(
+  width = if (landscape) height else width,
+  height = if (landscape) width else height,
+  deviceScaleFactor = device_info$dpr_js_css_pixel_ratio,
+  mobile = TRUE
 )
+
+b$Emulation$setUserAgentOverride(userAgent = device_info$user_agent)
+
+# b$Emulation$setTouchEmulationEnabled(enabled = TRUE)
+
+b$Page$navigate(paste0("http://", host, ":", port))
+
+b$view()
+
+# withr::with_options(
+#   list(
+#     browser = chromote::find_chrome()
+#   ),
+#   shinyMobile::preview_mobile(
+#     url = paste0("http://", host, ":", port),
+#     device = device,
+#     landscape = TRUE
+#   )
+# )
