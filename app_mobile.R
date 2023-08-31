7368da898fd23d7641ab630194f6c879321ce009
jaron94 <jonathan.aron94@gmail.com>
2023-08-31 11:29:24 +0100

    Avoid linting app_mobile.R

--- app_mobile.R
+++ app_mobile.R
@@ -1,5 +1,5 @@
 # Launch the ShinyApp (Do not remove this comment)
 
 pkgload::load_all(export_all = TRUE, helpers = FALSE, attach_testthat = FALSE)
-options(bgScorer.mobile = TRUE)
+options(bgScorer.mobile = TRUE) # nolint undesirable_function_linter
 run_app()
